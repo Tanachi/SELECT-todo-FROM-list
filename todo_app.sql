66420f32221c369380c27c25028818d0b7c709d2
Tanachi <tanachige@gmail.com>
2016-07-21 19:49:58 -1000

    Added select to display title and description

--- todo_app.sql
+++ todo_app.sql
@@ -5,11 +5,11 @@ CREATE DATABASE todo_app;
 GRANT ALL ON DATABASE todo_app TO Michael;
 CREATE TABLE tasks(
   id SERIAL,
-  title varcha(255) NOT NULL,
+  title varchar(255) NOT NULL,
   description text,
   created_at timestamp DEFAULT now(),
   updated_at timestamp,
-  completed boolean DEFAULT false;
+  completed boolean DEFAULT false
 );
 ALTER TABLE tasks ADD PRIMARY KEY(id);
 ALTER TABLE tasks DROP IF EXISTS completed;
@@ -17,7 +17,8 @@ ALTER TABLE tasks ADD completed_at timestamp DEFAULT NULL;
 ALTER TABLE tasks ALTER updated_at SET DEFAULT now();
 ALTER TABLE tasks ALTER updated_at SET NOT NULL;
 INSERT INTO tasks(title, description, created_at, updated_at, completed_at)
-  VALUES('STUDY SQL', 'Complete this exersise', now(), now());
+  VALUES('STUDY SQL', 'Complete this exersise', now(), now(), NULL);
 INSERT INTO tasks(title, description) VALUES ('Study Postgresql', 'Read all the documentation');
 SELECT * FROM tasks WHERE completed_at = NULL;
 UPDATE tasks SET completed_at = now() WHERE title = 'Study SQL';
+SELECT title, description FROM tasks WHERE completed_at = NULL;
