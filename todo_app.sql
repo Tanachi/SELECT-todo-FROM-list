18730677465815a6026d5ff696c4ce1d5fb135ec
Tanachi <tanachige@gmail.com>
2016-07-21 19:35:23 -1000

    Added table details

--- todo_app.sql
+++ todo_app.sql
@@ -3,4 +3,11 @@ CREATE USER Michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 DROP DATABASE IF EXISTS todo_app;
 CREATE DATABASE todo_app;
 GRANT ALL ON DATABASE todo_app TO Michael;
-CREATETABLE
+CREATE TABLE tasks(
+  id SERIAL,
+  title varcha(255) NOT NULL,
+  description text,
+  created_at timestamp DEFAULT now(),
+  updated_at timestamp,
+  completed boolean DEFAULT false;
+);
