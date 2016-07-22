0994277033d20999b208441c59cfaac68e876293
Tanachi <tanachige@gmail.com>
2016-07-21 19:39:53 -1000

    Added tasks

--- todo_app.sql
+++ todo_app.sql
@@ -16,3 +16,6 @@ ALTER TABLE tasks DROP IF EXISTS completed;
 ALTER TABLE tasks ADD completed_at timestamp DEFAULT NULL;
 ALTER TABLE tasks ALTER updated_at SET DEFAULT now();
 ALTER TABLE tasks ALTER updated_at SET NOT NULL;
+INSERT INTO tasks(title, description, created_at, updated_at, completed_at)
+  VALUES('STUDY SQL', 'Complete this exersise', now(), now());
+INSERT INTO tasks(title, description) VALUES ('Study Postgresql', 'Read all the documentation');
