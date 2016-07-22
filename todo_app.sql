f7a0deb92132495a1903973d20d3bf1827b0485d
Tanachi <tanachige@gmail.com>
2016-07-21 19:54:41 -1000

    Added Select to display title, description

--- todo_app.sql
+++ todo_app.sql
@@ -28,3 +28,4 @@ INSERT INTO tasks(title, description) VALUES ('mistake 2', 'another test entry')
 INSERT INTO tasks(title, description) VALUES ('mistake 3', 'another test entry');
 SELECT title FROM tasks WHERE title LIKE 'mistake%';
 DELETE FROM tasks WHERE title = 'mistake 1';
+SELECT title, description FROM tasks WHERE title LIKE 'mistake%';
